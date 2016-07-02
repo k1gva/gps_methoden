b54806dfa3a77ec0f1d0a4bdcf78bf8852879489
Saskia Geuking <k1.gva@wwu.de>
2016-07-02 12:05:19 +0200

    gpx jetzt als geojson umgewandelt durch https://github.com/JanKoppe/gps-analysis

--- map.js
+++ map.js
@@ -7,42 +7,26 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiazFndmEiLCJhIjoiWVhHMk5nOCJ9.hK5Z3lzyyhN4p5JwnFGVaQ'
 }).addTo(mymap);
 
-// Vergleichswert, kein Spiel 14.06. 15 Uhr
-var ohneSpiel = 'gpx/06_14_1500.gpx';
-new L.GPX(ohneSpiel, {async: true, polyline_options: {color: '#0000FF'}}).on('loaded', function(e) {
-    mymap.fitBounds(e.target.getBounds());
-}).addTo(mymap);
+var layerOhne = L.geoJson().addTo(mymap);
+var layerEngWal = L.geoJson().addTo(mymap);
 
-
-// Spiel Oesterreich - Ungarn 14.06. 18 Uhr
-var oestUng = new L.GPX(
-        'gpx/06_14_1800.gpx',
-       {async: true,
-        polyline_options: {
-            color: '#FF0000'}}).on('loaded', function(e){
-                mymap.fitbounds(e.target.getBounds());
-}).addTo(mymap);
-
-oestUng.on('loaded', function(e) {
-    var gpx = e.target,
-    distM = gpx.getdistance()
+// geojson Daten ohne Spiel
+var ohneSpiel = $.getJSON('geojson/06_14_1500.gpx.json', function(data) {
+    L.geoJson(data).addTo(layerOhne);
 });
-console.log(oestUng);
 
-// Spiel England - Wales, 16.06. 15 Uhr
-var engWal = 'gpx/06_16_1500.gpx';
-new L.GPX(engWal, {async: true, polyline_options: {color: '#FF0000'}}).on('loaded', function(e) {
-    mymap.fitbounds(e.target.getBounds());
-}).addTo(mymap);
+// geojson Daten Eng - Wal
+var engWal = $.getJSON('geojson/06_16_1500.gpx.json', function(data) {
+    L.geoJson(data).addTo(layerEngWal);
+});
 
-// Spiel Ukraine - Nordirland, 16.06. 18 Uhr
-var ukaNor = 'gpx/06_16_1800.gpx';
-new L.GPX(ukaNor, {async: true, polyline_options: {color: '#FFBF00'}}).on('loaded', function(e) {
-    mymap.fitbounds(e.target.getBounds());
-}).addTo(mymap);
+var baseMap = {
+    "Grundkarte": mymap
+};
 
-// Spiel Nordirland - Deutschland, 21.06. 18 Uhr
-var norDeu = 'gpx/06_21_1800.gpx';
-new L.GPX(norDeu, {async: true, polyline_options: {color: '#FFFF00'}}).on('loaded', function(e) {
-    mymap.fitbounds(e.target.getBounds());
-}).addTo(mymap);
+var overlayMaps = {
+    "Ohne Spiel": layerOhne,
+    "England - Wales": layerEngWal
+}
+
+L.control.layers(null, overlayMaps).addTo(mymap);
